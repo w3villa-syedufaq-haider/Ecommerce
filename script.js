049ea39ab5730e530aadbf93910e20dfc7343557
w3villa-syedufaq-haider <124652662+w3villa-syedufaq-haider@users.noreply.github.com>
2023-03-24 09:51:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ fetch("product.json")
           let items  = document.getElementById("product-list");
 
           let item = document.createElement("div");
+          
 
           item.innerHTML=`
           <div class="card position-relative m-4" style="width: 300px;">
@@ -18,7 +19,7 @@ fetch("product.json")
               <span class="top-left badge ">${product.off}</span>
           </div>
           <span
-              class="position-absolute top-10 start-100 translate-middle badge1  badge-danger">
+              class="position-absolute top-0 start-100 translate-middle badge1  badge-danger">
               ${product.discount}
           </span>
           <img src="${product.img}" class="card-img-top" width="100%" height="300px>
@@ -30,18 +31,20 @@ fetch("product.json")
                   <p class="d-flex flex-column text-muted mb-2">${product.model}
                   </p>
               </div>
+              <div id="buy">
               <strong class="pl-3">${product.name}</strong>
+              <p> <i class="fa-solid fa-dollar" style="color: lightgreen;"></i>${product.price} &nbsp; <s>${product.preprice}</s></p>
+              </div>
               <div class=" add mx-3 mt-3 d-block">
                   <input type="number" class="quantity__input" value="1">
-                  
-                  <button type="button" class="btn btn-danger btn-block mb-1"><small>ADD TO
-                          CART</small></button>&nbsp; &nbsp; &nbsp;
-                  <i class="fa-regular fa-heart mb-2"></i> &nbsp; &nbsp;
+
+                  <button type="button" class="btn btn-danger mb-1" onclick="getprodData(event,${product.id})">ADD TO
+                          CART</button>&nbsp; &nbsp; &nbsp;
+                <button type="button" class="btn btn-danger mb-1" onclick="getwishData(event,${product.id})"><i class="fa-regular fa-heart mb-2"></i></button> &nbsp; &nbsp;
                   <i class="fa-solid fa-arrow-right-arrow-left"></i>
               </div>
               <div class="d-flex flex-row justify-content-between p-3 mid">
                   <p class="d-flex flex-column mb-1">
-                    <p> <i class="fa-solid fa-dollar" style="color: lightgreen;"></i>${product.price} &nbsp; <s>${product.preprice}</s></p>
                     <p>Buy Now</p>
                   </p>
                   <p class="d-flex flex-column mb-2"><i class="fa-solid fa-question"
@@ -50,10 +53,134 @@ fetch("product.json")
               </div>
           </div>
       </div
-          
           `
           items.appendChild(item);
      }
 
 
 })
+
+
+async function getprodData(event,prodid){
+    event.preventDefault();
+
+    const response = await fetch("product.json");
+    const data = await response.json();
+
+        const product =   data.product.find(product=> prodid === product.id)
+        console.table(product);
+
+
+        let carts = document.getElementById("cart-container");
+
+
+
+        let cart = document.createElement("div");
+        
+        cart.innerHTML=`   
+        <table>
+        <tbody>
+            <tr>
+                <td><a href="#"><i class="fas fa-trash-alt fa-2x"></i></a></td>
+                <td><img src="${product.img}" alt=""></td>
+                <td class="w-25"><h5>${product.name}</h5></td>
+                <td><h5>${product.price}</h5></td>
+                <td><h5>1</h5></td>
+                <td><h5>${product.price}</h5></td>
+            </tr>     
+            
+        `
+        carts.appendChild(cart);
+}
+async function getwishData(event,prodid){
+    event.preventDefault();
+
+    const response = await fetch("product.json");
+    const data = await response.json();
+
+        const product =   data.product.find(product=> prodid === product.id)
+        console.table(product);
+
+
+        let wishes = document.getElementById("wish-container");
+
+
+
+        let wish = document.createElement("div");
+        
+        wish.innerHTML=`   
+        <table>
+        <tbody>
+            <tr>
+                <td><a href="#"><i class="fas fa-trash-alt fa-2x"></i></a></td>
+                <td><img src="${product.img}" alt=""></td>
+                <td class="w-25"><h5>${product.name}</h5></td>
+                <td><h5>${product.price}</h5></td>
+            </tr>     
+            
+        `
+        wishes.appendChild(wish);
+}
+
+
+
+
+
+// Register
+let umail = document.getElementById('email');
+let upass = document.getElementById('psw');
+let urepass = document.getElementById('psw-repeat');
+let add = document.getElementById('btnsubmit');
+
+let array = [];
+
+function register(e){
+         console.log(umail.value,upass.value,urepass.value);
+         let acceptData = () => {
+            array_user={
+              mail: umail.value,
+              pass: upass.value,
+              repass: urepass.value
+            };
+
+            array.push(array_user);
+
+            localStorage.setItem("array", JSON.stringify(array));
+          
+            console.log(array);
+
+
+          };
+        
+         let formValidation = () => {
+       
+            if (umail.value.trim() === "" || upass.value.trim() === "" || urepass.value.trim() === "") {
+              console.log("failure");
+              alert("Please fill all fields")
+            } 
+            else if(upass.value.trim() != urepass.value.trim()){
+                alert("password not match")
+            }
+              else {
+                console.log("success");
+                acceptData();   
+            }
+          };
+          formValidation();
+          document.getElementById("id01").style.display="none" ;     
+}
+function login(){
+   let enter_mail = document.getElementById('login-email').value ;
+   let enter_pass = document.getElementById('login-psw').value
+   let str = JSON.parse(localStorage.getItem("array"));
+   for(let i =0; i<str.length;i++){
+    if(enter_mail === str[i].mail && enter_pass === str[i].pass){
+        alert("success")
+    }
+    else{
+        alert("enter correct credentials")
+    }
+   }
+}
+
+
